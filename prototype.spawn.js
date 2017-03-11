03b1376e569ae83b7dde4a055cf0d47c8f454254
Tanner1638 <tanner1638@gmail.com>
2017-03-11 03:01:12 -0600

    Update prototype.spawn.js
    Added different types of custom creep spawns fitted for: Miner, Transport, and universal

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -1,6 +1,37 @@
 module.exports = function() {
     // create a new function for StructureSpawn
-    StructureSpawn.prototype.createCustomCreep =
+    StructureSpawn.prototype.createCustomCreepM =
+        function(energy, roleName) {
+            // create a balanced body as big as possible with the given energy
+            var numberOfParts = Math.floor(energy / 300);
+            var body = [];
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(WORK);
+            }
+            for (let i = 0; i < numberOfParts; i+=2) {
+                body.push(CARRY);
+            }
+            for (let i = 0; i < numberOfParts; i+=2) {
+                body.push(MOVE);
+            }
+            // create creep with the created body and the given role
+            return this.createCreep(body, undefined, { role: roleName, working: false });
+        };
+    StructureSpawn.prototype.createCustomCreepT =
+        function(energy, roleName) {
+            // create a balanced body as big as possible with the given energy
+            var numberOfParts = Math.floor(energy / 100);
+            var body = [];
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(CARRY);
+            }
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(MOVE);
+            }
+            // create creep with the created body and the given role
+            return this.createCreep(body, undefined, { role: roleName, working: false });
+        };
+    StructureSpawn.prototype.createCustomCreepU =
         function(energy, roleName) {
             // create a balanced body as big as possible with the given energy
             var numberOfParts = Math.floor(energy / 200);
@@ -14,7 +45,6 @@ module.exports = function() {
             for (let i = 0; i < numberOfParts; i++) {
                 body.push(MOVE);
             }
-
             // create creep with the created body and the given role
             return this.createCreep(body, undefined, { role: roleName, working: false });
         };
