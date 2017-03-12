627811783cf389bba638c2d1319cad696a414d78
Tanner1638 <tanner1638@gmail.com>
2017-03-12 03:54:23 -0500

    v0.2.1 PATCH

--- role.defender.js
+++ role.defender.js
@@ -1,44 +1,10 @@
-/* module.exports = {
-     a function to run the logic for this role
-    run: function(creep) {
-        // if creep is trying to complete a constructionSite but has no energy left
-        if (creep.memory.working == true) {
-            var target = creep.pos.findInRange(
-                FIND_HOSTILE_CREEPS
-                50);
-                if(target !== null);
-                    creep.rangedAttack(target);
-                else(
-                    creep.heal(creep));
-                    
-            
-            }
-        
-        
-            
-        } 
-};
-*/
-
 var roleUpgrader = require('role.upgrader');
 
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
         
-        /*
-        // if creep is trying to complete a constructionSite but has no energy left
-        if (creep.memory.working == true && creep.carry.energy == 0) {
-            // switch state
-            creep.memory.working = false;
-        }
-        // if creep is harvesting energy but is full
-        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-            // switch state
-            creep.memory.working = true;
-        }
-        */
-        // if creep is supposed to complete a constructionSite
+        // if creep working
         if (creep.memory.working == true) {
             // find closest enemy
             var enemy = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
@@ -56,7 +22,7 @@ module.exports = {
                 roleUpgrader.run(creep);
             }
             }
-        // if creep is supposed to harvest energy from source
+        // TEMPORARY ELSE STATEMENT untill i properly manage it
         else {
             var energy = creep.pos.findInRange(
                 FIND_DROPPED_ENERGY,
